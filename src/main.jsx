d9201e53ef50c2086722181844ccc667698ffac6
Mohamad <itisheidari@gmail.com>
2026-02-02 06:27:13 +0800

    offline and backup feature

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { registerSW } from 'virtual:pwa-register'
 import App from './App.jsx'
 import './index.css'
-
-// Register Service Worker for offline support
-registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
